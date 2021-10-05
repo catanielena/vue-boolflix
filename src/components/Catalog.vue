2fb161154c9dbccfa1fa99e30725ed4b16181bf2
catanielena <catani.elena1@gmail.com>
2021-10-05 12:32:18 +0200

    Add: structure

---
+++ src/components/Catalog.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>Catalog</p>
+</template>
+
+<script>
+export default {
+    name: "Catalog"
+    
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <input type="text" placeholder="Search">
+        <button type="button">Send</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+    
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
